6ec4df93d143b77f23d163d90e15fb4fdc2b4b28
Travis Frisinger <travis.frisinger@chillisoft.co.za>
2014-09-25 15:16:04 +0200

    Added splash screen

--- source/dist/platforms/android/assets/www/cordova_plugins.js
+++ source/dist/platforms/android/assets/www/cordova_plugins.js
@@ -13,13 +13,21 @@ module.exports = [
         "clobbers": [
             "plugins.appPreferences"
         ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.splashscreen/www/splashscreen.js",
+        "id": "org.apache.cordova.splashscreen.SplashScreen",
+        "clobbers": [
+            "navigator.splashscreen"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
     "com.borismus.webintent": "1.0.0",
-    "me.apla.cordova.app-preferences": "0.4.2"
+    "me.apla.cordova.app-preferences": "0.4.2",
+    "org.apache.cordova.splashscreen": "0.3.3"
 }
 // BOTTOM OF METADATA
 });
