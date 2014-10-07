4b57d81074293e103e923f79380727ecad5f4640
travis <tmfrisinger@gmail.com>
2014-10-07 21:07:47 +0200

    Tweeked to 1.0.2

--- source/dist/platforms/android/assets/www/cordova_plugins.js
+++ source/dist/platforms/android/assets/www/cordova_plugins.js
@@ -13,46 +13,13 @@ module.exports = [
         "clobbers": [
             "plugins.appPreferences"
         ]
-    },
-    {
-        "file": "plugins/org.apache.cordova.splashscreen/www/splashscreen.js",
-        "id": "org.apache.cordova.splashscreen.SplashScreen",
-        "clobbers": [
-            "navigator.splashscreen"
-        ]
-    },
-    {
-        "file": "plugins/de.appplant.cordova.plugin.local-notification/www/local-notification.js",
-        "id": "de.appplant.cordova.plugin.local-notification.LocalNotification",
-        "clobbers": [
-            "plugin.notification.local"
-        ]
-    },
-    {
-        "file": "plugins/com.google.cordova.admob/www/AdMob.js",
-        "id": "com.google.cordova.admob.AdMob",
-        "clobbers": [
-            "window.AdMob"
-        ]
-    },
-    {
-        "file": "plugins/org.apache.cordova.device/www/device.js",
-        "id": "org.apache.cordova.device.device",
-        "clobbers": [
-            "device"
-        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
     "com.borismus.webintent": "1.0.0",
-    "me.apla.cordova.app-preferences": "0.4.2",
-    "org.apache.cordova.splashscreen": "0.3.3",
-    "de.appplant.cordova.plugin.local-notification": "0.7.4",
-    "com.google.cordova.admob": "1.1.1",
-    "com.msopentech.websql": "0.0.4",
-    "org.apache.cordova.device": "0.2.13-dev"
+    "me.apla.cordova.app-preferences": "0.4.2"
 }
 // BOTTOM OF METADATA
 });
