091452125a30868effea0504005fc4679d3278c4
Travis Frisinger <travis.frisinger@chillisoft.co.za>
2014-11-11 08:30:14 +0200

    Release 1.1.0 - News Feature Published ;)

--- source/dist/platforms/android/assets/www/cordova_plugins.js
+++ source/dist/platforms/android/assets/www/cordova_plugins.js
@@ -15,9 +15,39 @@ module.exports = [
         ]
     },
     {
-        "file": "plugins/com.msopentech.indexedDB/www/IndexedDBShim.min.js",
-        "id": "com.msopentech.indexedDB.IndexedDBShim",
-        "runs": true
+        "file": "plugins/org.apache.cordova.splashscreen/www/splashscreen.js",
+        "id": "org.apache.cordova.splashscreen.SplashScreen",
+        "clobbers": [
+            "navigator.splashscreen"
+        ]
+    },
+    {
+        "file": "plugins/de.appplant.cordova.plugin.local-notification/www/local-notification.js",
+        "id": "de.appplant.cordova.plugin.local-notification.LocalNotification",
+        "clobbers": [
+            "plugin.notification.local"
+        ]
+    },
+    {
+        "file": "plugins/com.google.cordova.admob/www/AdMob.js",
+        "id": "com.google.cordova.admob.AdMob",
+        "clobbers": [
+            "window.AdMob"
+        ]
+    },
+    {
+        "file": "plugins/de.appplant.cordova.plugin.background-mode/www/background-mode.js",
+        "id": "de.appplant.cordova.plugin.background-mode.BackgroundMode",
+        "clobbers": [
+            "plugin.backgroundMode"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.device/www/device.js",
+        "id": "org.apache.cordova.device.device",
+        "clobbers": [
+            "device"
+        ]
     }
 ];
 module.exports.metadata = 
@@ -25,7 +55,12 @@ module.exports.metadata =
 {
     "com.borismus.webintent": "1.0.0",
     "me.apla.cordova.app-preferences": "0.4.2",
-    "com.msopentech.indexedDB": "0.1.1"
+    "org.apache.cordova.splashscreen": "0.3.3",
+    "de.appplant.cordova.plugin.local-notification": "0.7.4",
+    "com.google.cordova.admob": "1.1.1",
+    "com.msopentech.websql": "0.0.4",
+    "de.appplant.cordova.plugin.background-mode": "0.6.0-dev",
+    "org.apache.cordova.device": "0.2.13-dev"
 }
 // BOTTOM OF METADATA
 });
--- source/dist/www/scripts/998_app.js
+++ source/dist/www/scripts/998_app.js
@@ -1,34 +1,73 @@
 'use strict';
 (function() {
-    // WebHelper plugin shim import
-    // uncomment to open links in native browser
-    // var webHelper = window.WebHelper;
+    
     // example usage:
     // webHelper.openUrl("http://www.google.com")
 
-    // User Preferences plugin shim import
-    // uncomment to use preferences
-    
-    // example prefs usage:
-    // prefs.store(noop, noop, "key", "value");
-    // prefs.fetch(function (storedValue) {
-    // }, function (retrieveError) {
-    // }, "key");
     var app = {
         init: function() {
-            this._target = $("main");
-            this.restorePreferences();
+
+            // swap to false to show splash screen
+            var isDevEnv = false;
+            var loadScreenTTL = 5500;
+            var loadScreenInterval = 500;
+
             this.fixBottomMenuItemsForSmallerScreens();
-            this.show();
-			// register routing engine ;)
+            this.slashScreen(isDevEnv,loadScreenTTL, loadScreenInterval);
+		    this.bootstrap();	
+            // register routing engine ;)
 			var self = this;
 			document.addEventListener('click', function(){self.routingEngine(event);}, false);
         },
-        show: function() {
+        bootstrap : function(){
+
+            var viewService = new ViewService();
+            var settingsViewModel = new SettingsViewModel();
+            var marketViewModel = new MarketViewModel();
+            var newsViewModel = new NewsViewModel();
+            var newsService = new NewsService();
+            
+            var viewModel = new ViewModel(viewService, settingsViewModel, marketViewModel, newsViewModel, newsService);
+            var message = new MessageModel();
+            var loadViewModel = new UpdateProgress(10, 8);
+    
+            // -- load screen
+            this.bindLoader(message, loadViewModel);
+
+            // fetch data
+            newsService.fetchData(newsViewModel);
+            viewService.init(viewModel, settingsViewModel);
+    
+            // bind the ui
+            this.bindApp(settingsViewModel, marketViewModel, newsViewModel, viewModel);
+
+        },
+        bindLoader:function(message, loadViewModel){
+            setInterval(function(){loadViewModel.update(message);}, 400);
+            ko.applyBindings(message, document.getElementById("load"));
+        },
+        bindApp:function(settingsViewModel, marketViewModel, newsViewModel, viewModel){
+            // settings
+            ko.applyBindings(settingsViewModel, document.getElementById("defaults"));
+    
+            // -- main
+            viewModel.currency("USD"); // Text appears
+            ko.applyBindings(viewModel, document.getElementById("main"));
+            
+            // -- market 
+            ko.applyBindings(marketViewModel, document.getElementById("market"));
+            
+            // -- news
+            ko.applyBindings(newsViewModel, document.getElementById("newsRegion"));
+        },
+        slashScreen: function(isDevEnv, loadScreenTTL, loadScreenInterval) {
             // define splash and content -id elements for this functionality
-			//$('#main').show();
-			$('#load').collapse('show');
-			setTimeout(function(){$('#load').collapse('hide'); setTimeout(function(){$('#main').show();},500);},4500);
+            if(isDevEnv){
+			    $('#main').show();
+            }else{
+                $('#load').collapse('show');
+                setTimeout(function(){$('#load').collapse('hide'); setTimeout(function(){$('#main').show();},loadScreenInterval);},loadScreenTTL);    
+            }
         },
         fixBottomMenuItemsForSmallerScreens: function() {
             // if you have a ul.bottom, this helps to place it on smaller screens
@@ -42,9 +81,6 @@
             if (bottomListTop <= lastItemBottom) {
                 bottomList.css("position", "relative");
             }
-        },
-        restorePreferences: function() {
-            // TODO: restore user preferences here
         },
 		routingEngine:function(event){
 			// now we just need to handle routes and inject html fragments; )
