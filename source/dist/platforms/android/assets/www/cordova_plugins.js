e36494a94f1cb0ecb7121ef9c68211c5015aaa93
travis <tmfrisinger@gmail.com>
2014-11-11 19:39:17 +0200

    Added social share plugin

--- source/dist/platforms/android/assets/www/cordova_plugins.js
+++ source/dist/platforms/android/assets/www/cordova_plugins.js
@@ -15,31 +15,10 @@ module.exports = [
         ]
     },
     {
-        "file": "plugins/org.apache.cordova.splashscreen/www/splashscreen.js",
-        "id": "org.apache.cordova.splashscreen.SplashScreen",
+        "file": "plugins/nl.x-services.plugins.socialsharing/www/SocialSharing.js",
+        "id": "nl.x-services.plugins.socialsharing.SocialSharing",
         "clobbers": [
-            "navigator.splashscreen"
-        ]
-    },
-    {
-        "file": "plugins/de.appplant.cordova.plugin.local-notification/www/local-notification.js",
-        "id": "de.appplant.cordova.plugin.local-notification.LocalNotification",
-        "clobbers": [
-            "plugin.notification.local"
-        ]
-    },
-    {
-        "file": "plugins/com.google.cordova.admob/www/AdMob.js",
-        "id": "com.google.cordova.admob.AdMob",
-        "clobbers": [
-            "window.AdMob"
-        ]
-    },
-    {
-        "file": "plugins/org.apache.cordova.device/www/device.js",
-        "id": "org.apache.cordova.device.device",
-        "clobbers": [
-            "device"
+            "window.plugins.socialsharing"
         ]
     }
 ];
@@ -48,10 +27,7 @@ module.exports.metadata =
 {
     "com.borismus.webintent": "1.0.0",
     "me.apla.cordova.app-preferences": "0.4.2",
-    "org.apache.cordova.splashscreen": "0.3.3",
-    "de.appplant.cordova.plugin.local-notification": "0.7.4",
-    "com.google.cordova.admob": "1.1.1",
-    "org.apache.cordova.device": "0.2.13-dev"
+    "nl.x-services.plugins.socialsharing": "4.3.10"
 }
 // BOTTOM OF METADATA
 });
