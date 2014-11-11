012d64f6e95f23322b51595c5f8dfde0f36b3760
Travis Frisinger <travis.frisinger@chillisoft.co.za>
2014-11-11 12:49:37 +0200

    Removed background plugin

--- source/dist/platforms/android/assets/www/cordova_plugins.js
+++ source/dist/platforms/android/assets/www/cordova_plugins.js
@@ -36,11 +36,9 @@ module.exports = [
         ]
     },
     {
-        "file": "plugins/de.appplant.cordova.plugin.background-mode/www/background-mode.js",
-        "id": "de.appplant.cordova.plugin.background-mode.BackgroundMode",
-        "clobbers": [
-            "plugin.backgroundMode"
-        ]
+        "file": "plugins/com.msopentech.indexedDB/www/IndexedDBShim.min.js",
+        "id": "com.msopentech.indexedDB.IndexedDBShim",
+        "runs": true
     },
     {
         "file": "plugins/org.apache.cordova.device/www/device.js",
@@ -59,7 +57,7 @@ module.exports.metadata =
     "de.appplant.cordova.plugin.local-notification": "0.7.4",
     "com.google.cordova.admob": "1.1.1",
     "com.msopentech.websql": "0.0.4",
-    "de.appplant.cordova.plugin.background-mode": "0.6.0-dev",
+    "com.msopentech.indexedDB": "0.1.1",
     "org.apache.cordova.device": "0.2.13-dev"
 }
 // BOTTOM OF METADATA
