d068577ed0cfe415c2be7e6bdbc5f8454e3bd168
Travis Frisinger <travis.frisinger@chillisoft.co.za>
2014-11-17 14:44:00 +0200

    Added Additional News Source

--- backend/news/index.php
+++ backend/news/index.php
@@ -17,6 +17,7 @@
 	
 	$result = array();
 	// "http://bitcoinexaminer.org/feed/", 
+	// http://cointelegraph.com/rss
 	$feed = array("http://feeds.feedburner.com/BitcoinMagazine" ,"https://www.cryptocoinsnews.com/category/news/feed/", "http://feeds.feedburner.com/CoinDesk");
 	
 	foreach ($feed as &$value) {
